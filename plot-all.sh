2f1069baecb52aa3ecf2231b8cde98f781e0cb8f
Jonas Höchst <git@jonashoechst.de>
2016-08-31 18:26:40 +0200

    added jitter groups

--- plot-all.sh
+++ plot-all.sh
@@ -18,7 +18,7 @@ mv *.pdf pdfs
 echo
 echo "Plotting jitter groups"
 
-jitter_groups="n010 n025 n050 n100 unsteady rand"
+jitter_groups="n010 n025 n050 n100 maxfirst minfirst rand_and_sweet random static step_and_rand step unsteady"
 
 for g in $jitter_groups; do
     ./jitter-global.py $1/*$g*/
