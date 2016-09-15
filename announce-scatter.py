e0d908a5199693167b0823760933b1684694ed48
Jonas Höchst <git@jonashoechst.de>
2016-09-15 17:59:36 +0200

    added jitters for discrete values in scatter plot

--- announce-scatter.py
+++ announce-scatter.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-import sys, os, csv, re, collections, numpy
+import sys, os, csv, re, collections, numpy, random
 from CSVObject import *
 from plotter import *
 from mesherhelp import *
 
 import matplotlib.pyplot as plt
 from pprint import pprint
+random.seed(1)
+scatter_jitter = 0.8
 
 name = os.path.basename(os.path.normpath(sys.argv[1]))
 
@@ -18,16 +20,18 @@ x_values, aps = computeAnnouncesPerSecond(announces)
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.set_xlabel("time (s)")
-ax1.set_ylabel("node_announce", color="blue")
+ax1.set_ylabel("node announces", color="blue")
 
 for i in range(len(announces)):
-    plt.scatter(announces[i], [i] * len(announces[i]), marker="+", linewidth=0.2, color="blue")
+    # y_values = [i] * len(announces[i])
+    y_values = [i + 1 - (scatter_jitter/2) + scatter_jitter * random.random() for _ in range(len(announces[i]))]
+    plt.scatter(announces[i], y_values, marker="+", linewidth=0.2, color="blue")
 
 ax2 = ax1.twinx()
 ax2.set_ylabel("announces / second", color="orange", alpha=0.7)
 variableLine(ax2, x_values, aps, linewidth=1.5, alpha=0.7, color="orange")
 
-ax1.set_ylim([-1, len(announces)])
+ax1.set_ylim([0, len(announces)+1])
 ax1.set_xlim([0, end])
 plt.savefig(os.path.basename(__file__).split(".")[0]+"-"+name+".pdf")
 print("Plot is done.\n")
