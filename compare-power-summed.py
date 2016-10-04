69d8f99e2cb4cc83f131be638ae5d9452d41c2dd
Jonas Höchst <git@jonashoechst.de>
2016-10-04 14:13:54 +0200

    correct power summing

--- compare-power-summed.py
+++ compare-power-summed.py
@@ -16,25 +16,33 @@ experiments = [CSVObject(path+"/measurement.csv") for path in paths]
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.set_xlabel("time (s)")
-ax1.set_ylabel("energy consumtion (Ws)")
+ax1.set_ylabel("energy consumption (Wh)")
 
 import matplotlib.patches as mpatches
 patches = []
 
-end = 0
+global_end = 0
 
 for i in range(len(names)):
     color = colors[i % len(colors)]
     
-    start = experiments[i].get_values("timestamp_ms")[0] 
+    start = experiments[i].get_values("timestamp_ms")[0]
+    end = experiments[i].get_values("timestamp_ms")[-1] 
     x = [v-start for v in experiments[i].get_values("timestamp_ms")]
     data = experiments[i].get_values("power")
     
-    avg_power = 1.37910095889 #min(data)
-    # Die werte werden in Watt gemessen. Um die Energie menge zu kalkulieren, muss durch das Messintervall geteilt werden (5 messungen / sekunde)
-    summed = [sum([(val-avg_power)/5 for val in data[:j+1]]) for j in range(len(data))]
-    if end == 0: end = x[-1]
-    else: end = min(end, x[-1])
+    avg_power = 1.37910095889
+    # avg_power *= 0
+
+    duration = end - start
+    frequency = duration / len(x)
+    
+    summed = [sum([v-avg_power for v in data[:j+1]]) / (3600 / frequency) for j in range(len(x))]
+    if global_end == 0: end = x[-1]
+    else: global_end = max(end, x[-1])
+    
+    print("freq: {}".format(frequency))
+    print("finsum: {}".format(summed[-1]))
     
     factorAvgLine(ax1, summed, x=x, linewidth=1, alpha=0.7, color=color, factor=1)
     patches.append(mpatches.Patch(color=color, label=names[i], alpha=0.7))
